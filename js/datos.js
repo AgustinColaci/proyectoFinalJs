ba562d99514b28428320d7b794190593aa5eea9e
Agustín Colaci <agustincolaci@gmail.com>
2020-11-02 21:17:43 -0300

    Proyecto Final Js.

---
+++ js/datos.js
@@ -0,0 +1,28 @@
+/*Objeto JSON */
+
+const productos = [        
+    {
+     "nombre":"Cuaderno A4",
+     "precio":750,
+     "img":"img/cuaderno-a4.png",
+     }, {
+     "nombre":"Cuaderno A5",
+     "precio":650,
+     "img":"img/cuaderno-a5.png",
+     }, {
+     "nombre":"Cuaderno A6",
+     "precio":500,
+     "img":"img/cuaderno-a6.png",
+     }, {
+     "nombre":"Anotador",
+     "precio":150,
+     "img":"img/anotador.png",
+     }, {
+     "nombre":"Agenda",
+     "precio":500,
+     "img":"img/agenda.png",
+     }, {
+     "nombre":"Cuaderno Temático",
+     "precio":750,
+     "img":"img/tematico.png",
+     }, ]
